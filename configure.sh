ca0217fb6d41e5838b8a7e26ee7aec21a06de910
SKY <feixiangii683@gmail.com>
2021-12-27 13:30:06 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
-# V2Ray generate configuration
-# Download and install V2Ray
+# XRay generate configuration
+# Download and install XRay
 config_path=$PROTOCOL"_ws_tls.json"
-mkdir /tmp/v2ray
-curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
-unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
-install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
-install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
+mkdir /tmp/Xray
+curl -L -H "Cache-Control: no-cache" -o /tmp/Xray/Xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+unzip /tmp/Xray/Xray.zip -d /tmp/Xray
+install -m 755 /tmp/Xray/Xray /usr/local/bin/Xray
+install -m 755 /tmp/Xray/Xctl /usr/local/bin/Xctl
 # Remove temporary directory
-rm -rf /tmp/v2ray
-# V2Ray new configuration
-install -d /usr/local/etc/v2ray
-envsubst '\$UUID,\$WS_PATH' < $config_path > /usr/local/etc/v2ray/config.json
+rm -rf /tmp/Xray
+# XRay new configuration
+install -d /usr/local/etc/Xray
+envsubst '\$UUID,\$WS_PATH' < $config_path > /usr/local/etc/Xray/config.json
 # MK TEST FILES
 mkdir /opt/test
 cd /opt/test
 dd if=/dev/zero of=100mb.bin bs=100M count=1
 dd if=/dev/zero of=10mb.bin bs=10M count=1
-# Run V2Ray
-/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json &
+# Run XRay
+/usr/local/bin/Xray -config /usr/local/etc/Xray/config.json &
 # Run nginx
 /bin/bash -c "envsubst '\$PORT,\$WS_PATH' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'
