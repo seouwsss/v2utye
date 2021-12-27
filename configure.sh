17ae2043083c256ab573a8922769cac5f7db68bc
SKY <feixiangii683@gmail.com>
2021-12-27 15:15:37 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -6,7 +6,7 @@ mkdir /tmp/Xray
 curl -L -H "Cache-Control: no-cache" -o /tmp/Xray/Xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
 unzip /tmp/Xray/Xray.zip -d /tmp/Xray
 install -m 755 /tmp/Xray/Xray /usr/local/bin/Xray
-install -m 755 /tmp/Xray/Xctl /usr/local/bin/Xctl
+# install -m 755 /tmp/Xray/Xctl /usr/local/bin/Xctl
 # Remove temporary directory
 rm -rf /tmp/Xray
 # XRay new configuration
